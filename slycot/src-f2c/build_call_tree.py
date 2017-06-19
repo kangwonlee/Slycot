1b883a0ea07b76e8a25e7b0ccf97c5f648a8331c
autodrive <kangwon@gmail.com>
2017-06-20 05:22:08 +0900

    build calls tree

--- slycot/src-f2c/build_call_tree.py
+++ slycot/src-f2c/build_call_tree.py
@@ -1,20 +1,41 @@
 import subprocess
 import os
-
+import pprint
 
 
 class CFlow(object):
     def __init__(self, cflow_path=os.path.join('/', 'usr', 'bin', 'cflow')):
         self.cflow_path = cflow_path
+        self.calls_dict = {}
+        self.called_dict = {}
+        self.run_result = ''
 
     def run(self, cmd):
-        cp = subprocess.run([self.cflow_path, cmd])
-        print(cp.stdout)
+        cp = subprocess.run([self.cflow_path, cmd], stdout=subprocess.PIPE)
+        self.run_result = cp.stdout
+
+        result_lines = cp.stdout.decode().splitlines()
+
+        callee_list = None
+        key = 'del_this'
+
+        for line in result_lines:
+            if not line.startswith(' '):
+               # store result list
+               self.calls_dict[key] = str(callee_list)
+               # reset result list
+               key = line
+               callee_list = []
+            else:
+               callee_list.append(line.strip())
+
+        self.calls_dict.update({key:callee_list})
 
 
 def main():
     cflow = CFlow()
     cflow.run('SB02MT.c')
+    pprint.pprint(cflow.calls_dict)
 
 
 if '__main__' == __name__:
