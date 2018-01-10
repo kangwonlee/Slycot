734c71ead6aa4849f6ee3efe706a7b590ff092c2
autodrive <kangwon@gmail.com>
2018-01-10 13:37:29 +0900

    started F2cpReader()

--- slycot_reader/call_table_from_f2c_p.py
+++ slycot_reader/call_table_from_f2c_p.py
@@ -17,30 +17,34 @@ default_path_dict = {
 }
 
 
-def get_function_name_pattern():
-    return re.compile(r'.*\s+(.*?)\s*\(')
+class F2cpReader(object):
+    def __init__(self):
+        self.re_function_name = self.get_function_name_pattern()
 
+    @staticmethod
+    def get_function_name_pattern():
+        return re.compile(r'.*\s+(.*?)\s*\(')
 
-def parse_f2c_p(f2c_p_file_path):
-    with open(f2c_p_file_path) as f:
-        lines = f.readlines()
-    # first line : c definitions
-    # second line and after : list of other functions called
+    def parse_f2c_p(self, f2c_p_file_path):
+        with open(f2c_p_file_path) as f:
+            lines = f.readlines()
+        # first line : c definitions
+        # second line and after : list of other functions called
 
-    result = find_c_function_name(lines[0])
-    print(result.groups())
+        result = self.find_c_function_name(lines[0])
+        print(result.groups())
 
+    def find_c_function_name(self, f2c_p_first_line):
+        """
+        From the first line of the f2c P file, find function name using regex
 
-def find_c_function_name(f2c_p_first_line):
-    """
-    From the first line of the f2c P file, find function name using regex
-    :param str f2c_p_first_line: first line of f2c P file
-    :return:
-    """
-    r = get_function_name_pattern()
-    result = r.search(f2c_p_first_line)
-    return result
+        :param str f2c_p_first_line: first line of f2c P file
+        :return: function name string
+        """
+        result = self.re_function_name.search(f2c_p_first_line)
+        return result
 
 
 if __name__ == '__main__':
-    parse_f2c_p(os.path.join(default_path_dict['slycot']['f2c'], 'AB09AD.P'))
+    reader = F2cpReader()
+    reader.parse_f2c_p(os.path.join(default_path_dict['slycot']['f2c'], 'AB09AD.P'))
