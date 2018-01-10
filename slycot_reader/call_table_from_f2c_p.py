1e1fd6375dde6cccce69cdb0d7b1eec653d47fca
autodrive <kangwon@gmail.com>
2018-01-10 13:28:47 +0900

    refactored find_c_function_name() : from the first line of P file text, find the function name

--- slycot_reader/call_table_from_f2c_p.py
+++ slycot_reader/call_table_from_f2c_p.py
@@ -27,10 +27,19 @@ def parse_f2c_p(f2c_p_file_path):
     # first line : c definitions
     # second line and after : list of other functions called
 
-    r = get_function_name_pattern()
-
-    result = r.search(lines[0])
+    result = find_c_function_name(lines[0])
     print(result.groups())
+
+
+def find_c_function_name(f2c_p_first_line):
+    """
+    From the first line of the f2c P file, find function name using regex
+    :param str f2c_p_first_line: first line of f2c P file
+    :return:
+    """
+    r = get_function_name_pattern()
+    result = r.search(f2c_p_first_line)
+    return result
 
 
 if __name__ == '__main__':
