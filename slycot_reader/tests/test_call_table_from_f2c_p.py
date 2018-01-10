5287bf63e5f3fc3a18a9f071e2bafc96be2b8750
KangWon Lee <kangwon.lee@kpu.ac.kr>
2018-01-10 15:10:11 +0900

    TestF2cP : added tearDown() : delete self.reader object

--- slycot_reader/tests/test_call_table_from_f2c_p.py
+++ slycot_reader/tests/test_call_table_from_f2c_p.py
@@ -7,6 +7,9 @@ class TestF2cP(unittest.TestCase):
     def setUp(self):
         self.reader = cp.F2cpReader()
 
+    def tearDown(self):
+        del self.reader
+
     def test_get_function_name_pattern(self):
         # input
         sample_input = 'extern int ab09ad_(char *dico, char *job, char *equil, char *ordsel, integer *n, integer *m, integer *p, integer *nr, doublereal *a, integer *lda, doublereal *b, integer *ldb, doublereal *c__, integer *ldc, doublereal *hsv, doublereal *tol, integer *iwork, doublereal *dwork, integer *ldwork, integer *iwarn, integer *info, ftnlen dico_len, ftnlen job_len, ftnlen equil_len, ftnlen ordsel_len);'
