868c2f48f927b3509190566fc2f16b28942724dc
autodrive <kangwon@gmail.com>
2018-01-12 01:36:46 +0900

    included f2c.h

--- slycot/src-f2c/cos_cython_numpy.h
+++ slycot/src-f2c/cos_cython_numpy.h
@@ -1,4 +1,7 @@
 /*
     2.8.5.2. Numpy Support, 2.8.5. Cython, http://www.scipy-lectures.org/advanced/interfacing_with_c/interfacing_with_c.html#id13
 */
+
+#include "f2c.h"
+
 void cos_cython_numpy_func(double * in_array, double * out_array, int size);
